412a2e1081c8307a9482f24165a4109780057726
Sebastian Nagel <sebastian.nagel@ncoding.at>
2015-01-28 01:15:20 +0100

    Arrow completion, some keybinding and a basic prompt

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,10 +39,8 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
+bindkey '^[[1;5D' backward-word
+bindkey '^[[1;5C' forward-word
+bindkey '^[[1;5A' beginning-of-line
+bindkey '^[[1;5B' end-of-line
 bindkey '^[^N' newtab
-bindkey '^?' backward-delete-char
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,4 @@
 autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
 if (( $+commands[git] ))
 then
@@ -10,76 +8,53 @@ else
 fi
 
 git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
+  branch=$($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  if [[ $branch != "" ]]
   then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
+    echo "($branch)"
   fi
 }
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
+# git_dirty() {
+#   if $(! $git status -s &> /dev/null)
+#   then
+#     echo ""
+#   else
+#     if [[ $($git status --porcelain) == "" ]]
+#     then
+#       echo "on %{$fg_bold[green]%}$(git_branch)%{$reset_color%}"
+#     else
+#       echo "on %{$fg_bold[red]%}$(git_branch)%{$reset_color%}"
+#     fi
+#   fi
+# }
+
+# git_branch() {
+#  ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+#  echo "${ref#refs/heads/}"
+# }
+
+# git_unpushed() {
+#   unpushed=$($git cherry -v @{upstream} 2>/dev/null) || return
+#   if [[ $(unpushed) == "" ]]
+#   then
+#     echo " "
+#   else
+#     echo " %{$fg_bold[red]%}unpushed%{$reset_color%}"
+#   fi
+# }
+
+user() {
+  echo "%{$fg_bold[magenta]%}%n%{$reset_color%}"
 }
 
-unpushed () {
-  $git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-ruby_version() {
-  if (( $+commands[rbenv] ))
-  then
-    echo "$(rbenv version | awk '{print $1}')"
-  fi
-
-  if (( $+commands[rvm-prompt] ))
-  then
-    echo "$(rvm-prompt | awk '{print $1}')"
-  fi
-}
-
-rb_prompt() {
-  if ! [[ -z "$(ruby_version)" ]]
-  then
-    echo "%{$fg_bold[yellow]%}$(ruby_version)%{$reset_color%} "
-  else
-    echo ""
-  fi
+host() {
+  echo "%{$fg_bold[green]%}%m%{$reset_color%}"
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%{$fg_bold[cyan]%}%1/%\%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
-
-# Powerline
-. /usr/local/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh
+export PROMPT=$'$(user)@$(host):$(directory_name)$(git_branch) > '
+export RPROMPT="%{$fg_bold[grey]%}%t%{$reset_color%}"
