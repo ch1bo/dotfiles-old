c55fb3246debe5ca05934ee716838da0792c4864
Sebastian Nagel <sebastian.nagel@ncoding.at>
2015-02-11 00:02:57 +0100

    Added terminator config and switched prompt colors

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,49 +1,52 @@
 autoload colors && colors
 
+function colors() {
+  echo "256 colors"
+  for i in {0..255} ; do
+    echo "\x1b[38;5;${i}mcolour${i}"
+  done
+  echo "Solarized colors"
+  echo "$fg_bold[black]base03$reset_color"
+  echo "$fg[black]base02$reset_color"
+  echo "$fg_bold[green]base01$reset_color"
+  echo "$fg_bold[yellow]base00$reset_color"
+  echo "$fg_bold[blue]base0$reset_color"
+  echo "$fg_bold[cyan]base1$reset_color"
+  echo "$fg[white]base2$reset_color"
+  echo "$fg_bold[white]base3$reset_color"
+  echo "$fg[yellow]yellow$reset_color"
+  echo "$fg_bold[red]orange$reset_color"
+  echo "$fg[red]red$reset_color"
+  echo "$fg[magenta]magenta$reset_color"
+  echo "$fg_bold[magenta]violet$reset_color"
+  echo "$fg[blue]blue$reset_color"
+  echo "$fg[cyan]cyan$reset_color"
+  echo "$fg[green]green$reset_color"
+}
+
 # Context: user@hostname
 prompt_context() {
   local context
-  context+=%{$fg_bold[black]%}[%{$reset_color%}
-  context+=%{$fg_bold[green]%}%n%{$reset_color%}
+  context+=%{$fg_bold[green]%}[%{$reset_color%}
+  context+=%{$fg_bold[magenta]%}%n%{$reset_color%}
   if [[ -n "$SSH_CONNECTION" ]]; then
-    context=$context%{$fg_bold[magenta]%}@%m%{$reset_color%}
+    context=$context%{$fg[yellow]%}@%m%{$reset_color%}
   fi
-  context+=%{$fg_bold[black]%}]%{$reset_color%}
+  context+=%{$fg_bold[green]%}]%{$reset_color%}
   echo "$context"
 }
 
 # Dir: current working directory
 prompt_dir() {
   local dir
-  dir+=%{$fg_bold[black]%}[%{$reset_color%}
-  dir+=%{$fg_bold[blue]%}%~%{$reset_color%}
-  dir+=%{$fg_bold[black]%}]%{$reset_color%}
+  dir+=%{$fg_bold[green]%}[%{$reset_color%}
+  dir+=%{$fg[blue]%}%~%{$reset_color%}
+  dir+=%{$fg_bold[green]%}]%{$reset_color%}
   echo "$dir"
 }
 
 # Git: branch/detached head, dirty status
 prompt_git() {
-  local color ref
-  is_dirty() {
-    test -n "$(git status --porcelain --ignore-submodules)"
-  }
-  ref="$vcs_info_msg_0_"
-  if [[ -n "$ref" ]]; then
-    if is_dirty; then
-      color=yellow
-      ref="${ref} $PLUSMINUS"
-    else
-      color=green
-      ref="${ref} "
-    fi
-  if [[ "${ref/.../}" == "$ref" ]]; then
-    ref="$BRANCH $ref"
-  else
-    ref="$DETACHED ${ref/.../}"
-  fi
-  prompt_segment $color $PRIMARY_FG
-  print -Pn " $ref"
-  fi
 }
 
 # Status:
@@ -53,9 +56,9 @@ prompt_git() {
 prompt_status() {
   local symbols
   symbols=()
-  [[ $RETVAL -ne 0 ]] && symbols+="%{$fg_bold[red]%}✘ "
-  [[ $UID -eq 0 ]] && symbols+="%{$fg_bold[yellow]%}⚡ "
-  [[ $(jobs -l | wc -l) -gt 0 ]] && symbols+="%{$fg_bold[cyan]%}⚙ "
+  [[ $RETVAL -ne 0 ]] && symbols+="%{$fg[red]%}✘ "
+  [[ $UID -eq 0 ]] && symbols+="%{$fg_bold[red]%}⚡ "
+  [[ $(jobs -l | wc -l) -gt 0 ]] && symbols+="%{$fg[cyan]%}⚙ "
   echo "$symbols%{$reset_color%}"
 }
 
@@ -66,14 +69,6 @@ set_prompt() {
   symbols+="★ ❗ ⌥ ⎇  ⊢ ☢ ♻ ☀ ☁ ☔ ❄ "
   # Arrows
   symbols+="∝ ⌁ ♯ ≈ ➟ ➩ ➪ ⤳ ➟ ➤ ⇢ ➦ "
-
-  # echo $symbols
-  # for c in $fg; do
-  #   echo $c"normal"$reset_color
-  # done
-  # for c in $fg_bold; do
-  #   echo $c"bold"$reset_color
-  # done
 
   export PROMPT="$(prompt_context)$(prompt_dir)$(prompt_status)%{$fg_bold[  white]%} ➜  %{$reset_color%}"
   # RPROMPT="%{$fg_bold[red]%}❤ Bianca❤%{$reset_color%}"
