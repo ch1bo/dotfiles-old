37e9dc9411b2e6e24510b0d9649b375698c8e920
Sebastian Nagel <sebastian.nagel@ncoding.at>
2015-02-14 14:21:24 +0100

    Added gitconfig and sudo alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,17 +1,20 @@
+# Enable use of sudo with aliases
+alias sudo="nocorrect sudo "
+
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi  
-    
+    alias ls="ls --color=auto"
+
+    alias grep="grep --color=auto"
+    alias fgrep="fgrep --color=auto"
+    alias egrep="egrep --color=auto"
+fi
+
 # some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
+alias ll="ls -alF"
+alias la="ls -A"
+alias l="ls -CF"
 
 # List available hard disks
-alias disks='lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL'
+alias disks="lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL,UUID"
