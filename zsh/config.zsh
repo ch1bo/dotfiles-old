4c9e78f41d5554c1733d9a853e80fdb7f0bb59fa
Sebastian Nagel <sebastian.nagel@kastanienbaum.com>
2015-03-23 17:25:16 +0100

    Removed SHARE_HISTORY again

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -21,8 +21,8 @@ setopt CORRECT
 setopt COMPLETE_IN_WORD
 
 setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt INC_APPEND_HISTORY # adds history incrementally
+setopt HIST_IGNORE_ALL_DUPS # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
 zle -N newtab
