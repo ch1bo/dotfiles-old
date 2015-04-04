9f5baac2d0225e1b4db4655b12b546c9b77581a8
Sebastian Nagel <sebastian.nagel@ncoding.at>
2015-04-04 17:23:44 +0200

    Made completion of npm/grunt check for npm/grunt

--- js/completion.zsh
+++ js/completion.zsh
@@ -1,2 +1,6 @@
-eval "$(npm completion)"
-eval "$(grunt --completion=zsh)"
+if command -v npm >/dev/null 2>&1; then
+  eval "$(npm completion)"
+fi
+if command -v grunt >/dev/null 2>&1; then
+  eval "$(grunt --completion=zsh)"
+fi
