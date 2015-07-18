cf119e3b5aef4907d3ab1a1fdb2f2e34fe05d3da
Sebastian Nagel <sebastian.nagel@ncoding.at>
2015-07-18 20:52:23 +0200

    Added Xressources and urxvt config

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,41 +1,6 @@
 autoload colors && colors
 autoload -Uz vcs_info
 
-function colors() {
-  echo "===256 colors==="
-  for i in {0..255} ; do
-    echo "\x1b[38;5;${i}mcolour${i}"
-  done
-
-  echo "\n===8 colors==="
-  echo "$fg[black]black $fg_bold[black]bold$reset_color"
-  echo "$fg[red]red $fg_bold[red]bold$reset_color"
-  echo "$fg[green]green $fg_bold[green]bold$reset_color"
-  echo "$fg[blue]blue $fg_bold[blue]bold$reset_color"
-  echo "$fg[cyan]cyan $fg_bold[cyan]bold$reset_color"
-  echo "$fg[magenta]magenta $fg_bold[magenta]bold$reset_color"
-  echo "$fg[yellow]yellow $fg_bold[yellow]bold$reset_color"
-  echo "$fg[white]white $fg_bold[white]bold$reset_color"
-
-  echo "\n===Solarized colors==="
-  # echo "$fg_bold[black]base03$reset_color"
-  # echo "$fg[black]base02$reset_color"
-  # echo "$fg_bold[green]base01$reset_color"
-  # echo "$fg_bold[yellow]base00$reset_color"
-  # echo "$fg_bold[blue]base0$reset_color"
-  # echo "$fg_bold[cyan]base1$reset_color"
-  # echo "$fg[white]base2$reset_color"
-  # echo "$fg_bold[white]base3$reset_color"
-  echo "$fg[yellow]yellow$reset_color"
-  echo "$fg_bold[red]orange$reset_color"
-  echo "$fg[red]red$reset_color"
-  echo "$fg[magenta]magenta$reset_color"
-  echo "$fg_bold[magenta]violet$reset_color"
-  echo "$fg[blue]blue$reset_color"
-  echo "$fg[cyan]cyan$reset_color"
-  echo "$fg[green]green$reset_color"
-}
-
 # Context: user@hostname
 prompt_context() {
   local context
@@ -112,7 +77,7 @@ set_prompt() {
   # Arrows
   # symbols+="∝ ⌁ ♯ ≈ ➟ ➩ ➪ ⤳ ➟ ➤ ⇢ ➦ "
 
-  export PROMPT="$(prompt_context)$(prompt_dir)$(prompt_git)$(prompt_status)%{$fg[magenta]%} ➜ %{$reset_color%} "
+  export PROMPT="$(prompt_context)$(prompt_dir)$(prompt_git)$(prompt_status)%{$fg[magenta]%} ➜ %{$reset_color%}"
   # RPROMPT="%{$fg_bold[red]%}❤ Bianca❤%{$reset_color%}"
   # SPROMPT='zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? '
 }
