534587f1c03aedecc1d6d583a85a537c38a439c2
Sebastian Nagel <sebastian.nagel@ncoding.at>
2015-07-28 16:19:14 +0200

    Added dircolors and fixed fzf-z ordering again

--- fzf/config.zsh
+++ fzf/config.zsh
@@ -17,7 +17,7 @@ bindkey '^G' fzf-cd-widget
 # z (https://github.com/rupa/z) as source for fzf (only if available)
 fzf-z-widget() {
   if type "z" > /dev/null; then
-    cd "$(echo $(z -t -l | cut -d' ' -f2- | tr -d ' ' | fzf +s -e))"
+    cd "$(echo $(z -t -l | cut -d' ' -f2- | tr -d ' ' | fzf --tac +s -e))"
     zle reset-prompt
   fi
 }
