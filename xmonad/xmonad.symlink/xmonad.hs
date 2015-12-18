bc5165587375527c683381a76fd4161af4e7b579
Sebastian Nagel <sebastian.nagel@ncoding.at>
2015-12-18 20:01:12 +0100

    Added 2d navigation to xmonad Updated sublime packages

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,12 +1,15 @@
 import System.Exit
 import XMonad
+import XMonad.Actions.Navigation2D
 import XMonad.Hooks.DynamicLog
 
 import qualified Data.Map as M
 import qualified XMonad.StackSet as StackSet
 
-main = xmonad =<< xmobar' defaultConfig
-  -- Basics
+main = xmonad =<< xmobar' config'
+
+-- Basic configuration
+config' = navigation2D' $ defaultConfig
   { modMask            = mod4Mask  -- Super as modifier
   , terminal           = "urxvt"
   , focusFollowsMouse  = True      -- Focus on mouse enter
@@ -27,54 +30,12 @@ xmobar' = statusBar "xmobar" pp toggleStrutsKey
   -- Toggle display of top bar
   toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
+navigation2D' = withNavigation2DConfig defaultNavigation2DConfig
+
 -- Keybindings
 keyBindings' conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
-    -- launch a terminal
-    [ ((modMask,               xK_Return), spawn $ XMonad.terminal conf)
-
-    -- launch dmenu
-    , ((modMask,               xK_p     ), spawn "dmenu_run")
-    , ((modMask .|. shiftMask, xK_p     ), spawn "dmenu_desktop")
-
-    -- close focused window
-    , ((modMask              , xK_q     ), kill)
-
-     -- Rotate through the available layout algorithms
-    , ((modMask,               xK_space ), sendMessage NextLayout)
-
-    --  Reset the layouts on the current workspace to default
-    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-
-    -- Resize viewed windows to the correct size
-    , ((modMask,               xK_n     ), refresh)
-
-    -- Focus next/previous window in the stack
-    , ((modMask,               xK_Tab   ), windows StackSet.focusDown)
-    , ((modMask .|. shiftMask, xK_Tab   ), windows StackSet.focusUp)
-
-    -- Swap the focused window with the next window
-    , ((modMask .|. shiftMask, xK_j     ), windows StackSet.swapDown  )
-
-    -- Swap the focused window with the previous window
-    , ((modMask .|. shiftMask, xK_k     ), windows StackSet.swapUp    )
-
-    -- Focus the master window
-    , ((modMask,               xK_m     ), windows StackSet.focusMaster  )
-    -- Swap the focused window and the master window
-    , ((modMask .|. shiftMask, xK_m     ), windows StackSet.swapMaster)
-
-    -- Shrink/Expand the master area
-    , ((modMask,               xK_minus ), sendMessage Shrink)
-    , ((modMask,               xK_equal ), sendMessage Expand)
-
-    -- Increment/decremnet the number of windows in the master area
-    , ((modMask              , xK_comma ), sendMessage (IncMasterN 1))
-    , ((modMask              , xK_period), sendMessage (IncMasterN (-1)))
-
-    -- Push window back into tiling
-    , ((modMask,               xK_t     ), withFocused $ windows . StackSet.sink)
-
+    -- TODO: switch struts control
     -- Toggle the status bar gap
     -- Use this binding with avoidStruts from Hooks.ManageDocks.
     -- See also the statusBar function from Hooks.DynamicLog.
@@ -82,10 +43,70 @@ keyBindings' conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- , ((modMask              , xK_b     ), sendMessage ToggleStruts)
 
     -- Quit xmonad
-    , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
+    [ ((modMask .|. shiftMask, xK_q), io (exitWith ExitSuccess))
 
     -- Restart xmonad
-    , ((modMask .|. shiftMask, xK_r     ), spawn "xmonad --recompile; xmonad --restart")
+    , ((modMask .|. shiftMask, xK_r), spawn "xmonad --recompile; xmonad --restart")
+
+    -- Launch terminal
+    , ((modMask, xK_Return), spawn $ XMonad.terminal conf)
+
+    -- Launch dmenu
+    , ((modMask, xK_p), spawn "dmenu_run")
+    , ((modMask .|. shiftMask, xK_p), spawn "dmenu_desktop")
+
+    -- Close focused window
+    , ((modMask, xK_q), kill)
+
+     -- Rotate through the available layout algorithms
+    , ((modMask, xK_space ), sendMessage NextLayout)
+
+    --  Reset the layouts on the current workspace to default
+    , ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
+
+    -- Resize viewed windows to the correct size
+    , ((modMask, xK_n), refresh)
+
+    -- Focus next/previous window in the stack
+    , ((modMask, xK_Tab), windows StackSet.focusDown)
+    , ((modMask .|. shiftMask, xK_Tab), windows StackSet.focusUp)
+
+    -- Swap the focused window with the next window
+    , ((modMask .|. shiftMask, xK_j), windows StackSet.swapDown  )
+
+    -- Swap the focused window with the previous window
+    , ((modMask .|. shiftMask, xK_k), windows StackSet.swapUp    )
+
+    -- Focus the master window
+    , ((modMask, xK_m), windows StackSet.focusMaster  )
+    -- Swap the focused window and the master window
+    , ((modMask .|. shiftMask, xK_m), windows StackSet.swapMaster)
+
+    -- Shrink/Expand the master area
+    , ((modMask, xK_minus), sendMessage Shrink)
+    , ((modMask, xK_equal), sendMessage Expand)
+
+    -- Increment/decremnet the number of windows in the master area
+    , ((modMask, xK_comma), sendMessage (IncMasterN 1))
+    , ((modMask, xK_period), sendMessage (IncMasterN (-1)))
+
+    -- Push window back into tiling
+    , ((modMask, xK_t), withFocused $ windows . StackSet.sink)
+
+    -- Switch between layers (tiled, floating)
+    , ((modMask .|. controlMask, xK_Tab), switchLayer)
+
+    -- Directional navigation of windows
+    , ((modMask, xK_l), windowGo R False)
+    , ((modMask, xK_h), windowGo L False)
+    , ((modMask, xK_k), windowGo U False)
+    , ((modMask, xK_j), windowGo D False)
+
+    -- Swap adjacent windows
+    , ((modMask .|. shiftMask, xK_l), windowSwap R False)
+    , ((modMask .|. shiftMask, xK_h), windowSwap L False)
+    , ((modMask .|. shiftMask, xK_k), windowSwap U False)
+    , ((modMask .|. shiftMask, xK_j), windowSwap D False)
     ]
     ++
 
