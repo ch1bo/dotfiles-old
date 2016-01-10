c83f6947e08e170c4f751ce9ac7732f17134a731
Sebastian Nagel <sebastian.nagel@ncoding.at>
2016-01-10 11:35:49 +0100

    Removed terminator config

--- private/env.zsh
+++
@@ -1,7 +0,0 @@
-# Stash your environment variables in ~/.env.zsh. This means they'll stay out
-# of your main dotfiles repository (which may be public, like this one), but
-# you'll have access to them in your scripts.
-if [[ -a ~/.env.zsh ]]; then
-  source ~/.env.zsh
-fi
-
